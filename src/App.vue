de032ad372ba08679f85ab35347032ee018f7e57
Jdionisr <148556552+Jdionisr@users.noreply.github.com>
2023-10-20 15:41:18 +0200

    Subida componentes

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+import BlogPost from "./components/BlogPost.vue";
+import ButtonCounter from "./components/ButtonCounter.vue";
+</script>
+
+<template>
+  <div class="bg-body-tertiary">
+    <h1>APP</h1>
+    <ButtonCounter />
+    <BlogPost title="Post 1" id="1" body="Description 1" colorText="primary" />
+    <BlogPost
+      title="Post 2"
+      id="2"
+      body="Description 2"
+      colorText="secondary"
+    />
+    <BlogPost title="Post 3" id="3" body="Description 3" colorText="success" />
+    <BlogPost title="Post 4" id="4" body="Description 4" colorText="info" />
+  </div>
+</template>
+
+<style></style>
---
+++ src/components/BlogPost.vue
@@ -0,0 +1,15 @@
+<script setup>
+defineProps(["title", "id", "body", "colorText"]);
+</script>
+
+<template>
+  <div class="card">
+    <div class="card-body">
+      <h5 class="card-title" :class="`text-${colorText}`">
+        {{ id }} - {{ title }}
+      </h5>
+      <h6 class="card-subtitle">{{ body }}</h6>
+      <p>Lorem ipsum dolor sit amet.</p>
+    </div>
+  </div>
+</template>
